53a3bbcb338d403eac52549204aff261bc41cbff
LeHuuTrong7 <tronglehuu912@gmail.com>
2024-08-16 11:11:09 -0700

    Commit and Push

---
+++ src/Welcom.java
@@ -0,0 +1,6 @@
+public class Welcom {
+    public static void main(String[] args) {
+        System.out.println("Hello Le Huu Trong!");
+        System.out.println("Welcome to the DHKTPM19A!");
+        }
+    }
